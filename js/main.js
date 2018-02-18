55645a094a7c3265c07faff123c8db7c3234ed96
Nemanja Blagojević <neccymaradona@yahoo.com>
2018-02-18 01:35:32 +0100

    js app

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
 $(document).ready(function(){
 
+  $("#text2").hide();
+  $("#text3").hide();
+
   $("#pro1").mouseenter(function(){
-    $("#text1").addClass("active");
-    $("#text2").removeClass("active").addClass("hidden");
-    $("#text3").removeClass("active").addClass("hidden");
+    $("#text1").show();
+    $("#text2").hide();
+    $("#text3").hide();
   });
 
   $("#pro2").mouseenter(function(){
-    $("#text2").addClass("active");
-    $("#text1").removeClass("active").addClass("hidden");
-    $("#text3").removeClass("active").addClass("hidden");
+    $("#text2").show();
+    $("#text1").hide();
+    $("#text3").hide();
   });
 
   $("#pro3").mouseenter(function(){
-    $("#text3").addClass("active");
-    $("#text2").removeClass("active").addClass("hidden");
-    $("#text1").removeClass("active").addClass("hidden");
+    $("#text3").show();
+    $("#text2").hide();
+    $("#text1").hide();
   });
+
+
 });

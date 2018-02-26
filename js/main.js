fc9ec74eeb82ccf2a5d0b1ab782ea8b953037d88
Nemanja Blagojević <neccymaradona@yahoo.com>
2018-02-27 00:42:51 +0100

    detalji

--- js/main.js
+++ js/main.js
@@ -102,22 +102,112 @@ $("#pcela").click(function(){
     $(".pcela").show();
   });
 
+// --------- DETALJNIJE ----------
+
+//-----------NAPICI------------
+$(".joost-pineapple").click(function() {
+    $(".joost-pineapple" + ".modal" ).fadeToggle();
+});
+
+$(".joost-blueberry").click(function() {
+    $(".joost-blueberry" + ".modal" ).fadeToggle();
+});
+
+$(".herbal-tea").click(function() {
+    $(".herbal-tea" + ".modal" ).fadeToggle();
+});
+
+$(".berry-nectar").click(function() {
+    $(".berry-nectar" + ".modal" ).fadeToggle();
+});
+
+$(".aloe-vera-gel").click(function() {
+    $(".aloe-vera-gel" + ".modal" ).fadeToggle();
+});
+
+$(".pomesteen-power").click(function() {
+    $(".pomesteen-power" + ".modal" ).fadeToggle();
+});
+
+$(".bits-n-peaches").click(function() {
+    $(".bits-n-peaches" + ".modal" ).fadeToggle();
+});
+
+$(".forever-freedom").click(function() {
+    $(".forever-freedom" + ".modal" ).fadeToggle();
+});
+
+$(".aloe2go").click(function() {
+    $(".aloe2go" + ".modal" ).fadeToggle();
+});
+//------------- PCELINJI PROIZVODI---------
+$(".bee-pollen").click(function() {
+    $(".bee-pollen" + ".modal" ).fadeToggle();
+});
+
+$(".bee-propolis").click(function() {
+    $(".bee-propolis" + ".modal" ).fadeToggle();
+});
+
+$(".royal-jelly").click(function() {
+    $(".royal-jelly" + ".modal" ).fadeToggle();
+});
+
+//-------- DODACI ISHRANI -----------
+$(".forever-kids").click(function() {
+    $(".forever-kids" + ".modal" ).fadeToggle();
+});
+
+$(".field-of-greens").click(function() {
+    $(".field-of-greens" + ".modal" ).fadeToggle();
+});
+
+$(".forever-daily").click(function() {
+    $(".forever-daily" + ".modal" ).fadeToggle();
+});
+
+$(".absorbent-c").click(function() {
+    $(".absorbent-c" + ".modal" ).fadeToggle();
+});
+
+$(".nature-min").click(function() {
+    $(".nature-min" + ".modal" ).fadeToggle();
+});
+
+$(".gin-chia").click(function() {
+    $(".gin-chia" + ".modal" ).fadeToggle();
+});
+
+$(".garlic-thyme").click(function() {
+    $(".garlic-thyme" + ".modal" ).fadeToggle();
+});
+
+$(".multi-maca").click(function() {
+    $(".multi-maca" + ".modal" ).fadeToggle();
+});
+
+$(".arctic-sea").click(function() {
+    $(".arctic-sea" + ".modal" ).fadeToggle();
+});
+
+$(".a-beta-care").click(function() {
+    $(".a-beta-care" + ".modal" ).fadeToggle();
+});
+
+$(".esm-complex").click(function() {
+    $(".esm-complex" + ".modal" ).fadeToggle();
+});
+
+
+
 // ---------- MEDIA QUERY - PROIZVODI -------------
 
 $(".btn1").hide();
-$(".btn2").hide();
 
 $('.btn1').on('click', function(){
-    $('.menu').css("width", "100%");
-    $(".btn1").hide();
-    $(".btn2").show();
+    $('.menu').toggleClass("pop");
+    $(this).toggleClass("hamburger");
   });
-
-  $('.btn2').on('click', function(){
-      $('.menu').css("width", "0");
-      $(".btn2").hide();
-      $(".btn1").show();
-    });
 
   // --------- dropbar ---------
   $('#logo2').on('click', function(){
@@ -130,17 +220,6 @@ if ($(window).width() < 960) {
    $("#text3").show();
    $(".btn1").show();
 
-   $('.btn1').on('click', function(){
-       $('.menu').css("width", "100%");
-       $(".btn1").hide();
-       $(".btn2").show();
-     });
-
-     $('.btn2').on('click', function(){
-         $('.menu').css("width", "0");
-         $(".btn2").hide();
-         $(".btn1").show();
-       });
 }
 
 
